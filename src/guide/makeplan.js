a3bab9b1a865875113e596ecf4e87529d78487d2
JuheeNoh123 <sarah19270@naver.com>
2024-04-04 17:36:46 +0900

    주차 나눠주는 기능  함수로 분리

--- src/guide/makeplan.js
+++ src/guide/makeplan.js
@@ -13,22 +13,42 @@ req
     
 }
 */
-// async function makeextra(category_cnt,category_Id){
-    
-//     for(let i=0;i<category_cnt;i++){
-//         let guide = new guide_model('', category_Id[i]); 
-//         const extra_guide = await guide.find();
-//         const pr_extra_guide = extra_guide[0];
-//         //가이드 리스트들을 라우터로 어떻게 보내지..?
-//         pr_extra_guide
-//     }
-// }
+const sum = [];
+const final_list = [];
 
 async function randomInt(min, max){ 
-    console.log("min: ", min);
-    console.log("max: ", max);
+    // console.log("min: ", min);
+    // console.log("max: ", max);
     var randomNum = Math.floor(Math.random()*(max-min+1)) + min; 
     return randomNum;
+}
+
+async function shuffle(array) {
+    for (let index = array.length - 1; index > 0; index--) {
+      // 무작위 index 값을 만든다. (0 이상의 배열 길이 값)
+      const randomPosition = Math.floor(Math.random() * (index + 1));
+  
+      // 임시로 원본 값을 저장하고, randomPosition을 사용해 배열 요소를 섞는다.
+      const temporary = array[index];
+      array[index] = array[randomPosition];
+      array[randomPosition] = temporary;
+    }
+}
+  
+async function splitweeks(final_list){
+    let transformedData = [];
+    const groupedByFive = [];
+    let count = 1;
+
+    for (let i = 0; i < final_list.length; i += 5) {
+        const group = {};
+        group[count++] = final_list.slice(i, i + 5).map((item, index) => ({[`${index + 1}`]: item.guide_NM}));
+        groupedByFive.push(group);
+    }
+    
+    transformedData = JSON.stringify(groupedByFive);
+    return transformedData;
+
 }
 
 router.post('/makeplan', async(req,res)=>{
@@ -36,9 +56,7 @@ router.post('/makeplan', async(req,res)=>{
     const listcnt= guide_list.length;
     const category_Id = req.body.checklist.category_Id;
     const category_cnt = category_Id.length;
-    const final_list = [];
-    const sum = [];
-
+    
     for(let i=0;i<guide_list.length;i++){
         let guide = new guide_model();
         const add_guideNM = await guide.findwithguideId(guide_list[i]);
@@ -53,47 +71,32 @@ router.post('/makeplan', async(req,res)=>{
             //console.log(pr_extra_guide);        //배열 이쁘게 만들어주기
             sum.push(pr_extra_guide);       //하나의 배열로 합쳐주기
         }
-
         let cnt = 0;
         
         while(cnt<(25-listcnt)){
-            for(let i=0; i<sum.length;i++){
-                if(cnt<(25-listcnt)){
-                    const idx1 = await randomInt(0, category_cnt-1);
-                    const idx2 = await randomInt(0, sum[i].length-1);
-                    console.log(idx1, idx2);
-                    cnt++;
-
-                    final_list.push(sum[idx1][idx2]);
-                }
-                else{break;}
-            }
-            //console.log("final_list",final_list);
+            const idx1 = await randomInt(0, category_cnt-1);
+            const idx2 = await randomInt(0, sum[idx1].length-1);
+            console.log(idx1, idx2);
+            cnt++;
+            final_list.push(sum[idx1][idx2]);
         }
         
-
+        
     }
+    console.log("전\n",final_list);
+
+    await shuffle(final_list);
 
     //final_list.push()
-    console.log(final_list);
-
-    // 변환된 결과를 담을 배열
-    let transformedData = [];
-
-    // 요구 사항에 따라 데이터를 그룹화
-    const groups = {};
-
-    const groupedByFive = [];
-    let count = 1;
-
-    for (let i = 0; i < final_list.length; i += 5) {
-        const group = {};
-        group[count++] = final_list.slice(i, i + 5).map((item, index) => ({[`${index + 1}`]: item.guide_NM}));
-        groupedByFive.push(group);
-    }
+    console.log('후\n',final_list);
+    console.log(final_list.length);
     
-    transformedData = JSON.stringify(groupedByFive);
+    const transformedData = await splitweeks(final_list);
     res.send(transformedData);
     
+})
+
+router.post('/delete', async(req,res)=>{
+
 })
 module.exports = router;
--- test.js
+++ test.js
@@ -16,24 +16,63 @@ guide_Id_list= [
   ];
 
   
-async function test(){
-    var data = [];
-    for(let i=0;i<add_data.length;i++){
-        //let guide = new guide_model();
-        //let guide_Id_list =guide.findwithguide_NM(add_data[i]);
-        let guide_Id = guide_Id_list[0][0].guide_Id;    //222
-        //console.log("guide_Id : ",guide_Id);
-        //data.$[{guide_Id}] = add_data[i];   //add_data[i] : '지구를 위해 무엇이든 할 것을 결심하기' 
-        var jsonObject = {
-            'guide_Id': guide_Id,
-            'guide_NM': add_data[i]
-        }
+// async function test(){
+//     var data = [];
+//     for(let i=0;i<add_data.length;i++){
+//         //let guide = new guide_model();
+//         //let guide_Id_list =guide.findwithguide_NM(add_data[i]);
+//         let guide_Id = guide_Id_list[0][0].guide_Id;    //222
+//         //console.log("guide_Id : ",guide_Id);
+//         //data.$[{guide_Id}] = add_data[i];   //add_data[i] : '지구를 위해 무엇이든 할 것을 결심하기' 
+//         var jsonObject = {
+//             'guide_Id': guide_Id,
+//             'guide_NM': add_data[i]
+//         }
 
-        data.push(jsonObject);
+//         data.push(jsonObject);
         
-    }
+//     }
 
-    console.log("data : ",data);
+//     console.log("data : ",data);
+// }
+
+// test();
+array = [
+    { guide_NM: '대중교통 이용' },
+    { guide_NM: '자전거 나 걷기' },
+    { guide_NM: '전기 자동차 구매 고려' },
+    { guide_NM: '친환경 제품 구매' },
+    { guide_NM: '부동산 재활용 제품 구매' },
+    { guide_NM: '지구 친화적 제품 선호' },
+    { guide_NM: '지구 친화적 옷차림' },
+    { guide_NM: '중고 제품 구매' },
+    { guide_NM: '유기농 제품 선호' },
+    { guide_NM: '가공 식품 줄이기' },
+    { guide_NM: '비건 식단 고려' },
+    { guide_NM: '지역 시장 이용' },
+    { guide_NM: '올바른 제품 포장' },
+    { guide_NM: '친환경 차량 구입하기' },
+    { guide_NM: '부동산 재활용 제품 구매' },
+    { guide_NM: '친환경 제품 구매' },
+    { guide_NM: '장거리 여행시 대중교통을 선호하기' },
+    { guide_NM: '친환경 제품 구매' },
+    { guide_NM: '친환경 차량 구입하기' },
+    { guide_NM: '유기농 제품 선호' },
+    { guide_NM: '필요한 만큼만 구매' },
+    { guide_NM: '식재료를 재활용하여 음식을 만들기' },
+    { guide_NM: '식사 계획 세우기' },
+    { guide_NM: '육류 소비를 줄이고 채소 중심 식습관을 가지기' },
+    { guide_NM: '자가용 대신 대중교통을 이용하거나 동승하고 서로 간 배려하기' }
+  ];
+
+for (let index = array.length - 1; index > 0; index--) {
+    // 무작위 index 값을 만든다. (0 이상의 배열 길이 값)
+    const randomPosition = Math.floor(Math.random() * (index + 1));
+
+    // 임시로 원본 값을 저장하고, randomPosition을 사용해 배열 요소를 섞는다.
+    const temporary = array[index];
+    array[index] = array[randomPosition];
+    array[randomPosition] = temporary;
 }
 
-test();
+console.log(array);
